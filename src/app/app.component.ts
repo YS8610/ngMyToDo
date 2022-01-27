390c6eb8bc127be936ddc3d3f154eee53bf629e0
YS_workPC <ong_ys86@yahoo.com.sg>
2022-01-28 01:14:01 +0800

    add list, mark completed, delete function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngMyToDo';
+
+
+  todoForm !: FormGroup;
+  listofTodo : {desc:string , priority:string, due :Date }[] = [];
+
+
+  constructor(private fb : FormBuilder){};
+
+  ngOnInit(): void {
+    this.todoForm = this.fb.group(
+      {
+        desc : this.fb.control('',Validators.required),
+        priority : this.fb.control('',Validators.required),
+        due : this.fb.control('',Validators.required)
+      }
+    );
+  }
+
+  addTask(){
+    const todo = this.todoForm.value;
+    const todayString = new Date().toISOString();
+    const todayDate = new Date(todayString.substring(0,10));
+    const dueDate = new Date(todo.due);
+
+    if (dueDate.getTime() < todayDate.getTime()){
+      alert("Task with due date in the past cannot be added");
+    }
+    else{
+      this.listofTodo.push({
+        desc:todo.desc,
+        priority:todo.priority,
+        due: todo.due
+      });
+      this.todoForm.reset();
+    }
+  }
+
+  removeTask(e:number){
+    console.log(e);
+    this.listofTodo.splice(e,1);
+  }
 }
---
+++ src/app/task/task.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+
+  @Input() t !:{desc:string, priority: string, due:Date};
+  @Input() ind !: number;
+  @Output() onDelete = new EventEmitter<number>();
+
+  isDone = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDone(e:Event){
+    this.isDone = (<HTMLInputElement>e.target).checked;
+  }
+
+  deleteTask(){
+    this.onDelete.emit(this.ind);
+  }
+}
